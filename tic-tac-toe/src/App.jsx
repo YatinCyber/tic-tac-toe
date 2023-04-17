87bf1862438efd52ab1c60cbb5e89c4b53cb89cc
YatinCyber <chabonglatui@gmail.com>
2023-04-17 21:26:09 +0700

    Create cell board

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -4,11 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(""));
+  const [turn, setTurn] = useState("x");
+  //win = null - "available"
+  const [win, setWin] = useState(null);
 
   return (
     <div >
-      HI, THIS IS TIC TAC TOE PAGE DEMO 
-      CREATED BY TINNG
+      <h1> BOARD - GAME </h1>
+      <button>hello game</button>
+
+      <div className="wrapper">
+        <h1>wrapper</h1>
+        {Array.from("012345678").map((ind) => (
+          <button 
+          key={ind}
+          onClick={()=> console.log(ind)}
+          >Cell {ind}</button>
+          
+                ))}
+      </div>
     </div>
   )
 }
